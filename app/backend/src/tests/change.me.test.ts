3ab4b26093d01505d9f3db82c8efce71b7d487bc
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-22 21:44:21 -0300

    test: implementa testes rota /login.

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -12,7 +12,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Teste da rota POST /login', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -38,8 +38,43 @@ describe('Seu teste', () => {
 
   //   expect(...)
   // });
+  describe('deve retornar um status 400', () => {
+    it('quando o campo "email" não for informado', async () => {
+      const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ password: 'any_password' })
+      expect(httpResponse.status).to.be.eq(400);
+      expect(httpResponse.body).to.deep.equal({ message: 'All fields must be filled' });
+    });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+    it('quando o campo "senha" não for informado', async () => {
+      const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'any-email@any.com' })
+      expect(httpResponse.status).to.be.eq(400);
+      expect(httpResponse.body).to.deep.equal({ message: 'All fields must be filled' });
+    })
+  })
+
+  describe('deve retornar um status 401', () => {
+    it('quando o campo "email" estiver incorreto', async () => {
+      const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'any-email@any.com', password: 'secret_admin' })
+      expect(httpResponse.status).to.be.eq(401);
+      expect(httpResponse.body).to.deep.equal({ message: 'Incorrect email or password'})
+    });
+
+    it('quando o campo "password" estiver incorreto', async () => {
+      const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 'any_password' })
+      expect(httpResponse.status).to.be.eq(401);
+      expect(httpResponse.body).to.deep.equal({ message: 'Incorrect email or password'})
+    })
+  })
 });
