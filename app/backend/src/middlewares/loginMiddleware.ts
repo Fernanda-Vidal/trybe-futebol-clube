e9c31cac9582743827609f7a9f214b4cb2d63529
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-23 14:18:57 -0300

    chore: implementa regEx email.

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -4,10 +4,20 @@ import {
 } from 'http-status-codes';
 import HttpException from '../utils/HttpException';
 
+const isValidEmail = (email: string) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
 export default function loginMiddleware(req: Request, _res: Response, next: NextFunction) {
   const { email, password } = req.body;
   if (!email || !password) {
     throw new HttpException('All fields must be filled', StatusCodes.BAD_REQUEST);
   }
+
+  if (!isValidEmail) {
+    throw new HttpException('Incorrect email or password', StatusCodes.UNAUTHORIZED);
+  }
+
   next();
 }
