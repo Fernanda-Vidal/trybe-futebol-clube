b30fca443c0297854569d834e3fb3ed4eebf548a
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-27 10:46:56 -0300

    feat: valida se todos os times existem.

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -6,10 +6,13 @@ import HttpException from '../utils/HttpException';
 
 export default class MatchService implements IMatchService {
   private model = Match;
+  private teamModel = Team;
+
   constructor() {
     this.getAllMatches = this.getAllMatches.bind(this);
     this.getByInProgress = this.getByInProgress.bind(this);
     this.createMatch = this.createMatch.bind(this);
+    this.updateMatch = this.updateMatch.bind(this);
   }
 
   async getAllMatches(): Promise<Match[]> {
@@ -37,9 +40,37 @@ export default class MatchService implements IMatchService {
     return searchMatches;
   }
 
+  async getTeam(ids: number[]): Promise<any> {
+    const idExist = await (await (await Promise.all(ids.map((id) => this.teamModel.findByPk(id))))
+      .every((team) => team !== null));
+    console.log('getTeam', idExist);
+    if (!idExist) return false;
+    return true;
+  }
+
   async createMatch(req: IReqMatch): Promise<INewMatch> {
+    const { homeTeam, awayTeam } = req;
+
+    const message = 'There is no team with such id!';
+    const teamExists = await this.getTeam([Number(homeTeam), Number(awayTeam)]);
+
+    if (!teamExists) throw new HttpException(message, StatusCodes.NOT_FOUND);
+
     const newMatch = { ...req, inProgress: true };
     const match = await this.model.create(newMatch);
     return match;
+  }
+
+  async updateMatch(id: number): Promise<boolean> {
+    const idExists = await this.model.findByPk(id);
+    if (!idExists) throw new HttpException('There is no match with such id', StatusCodes.NOT_FOUND);
+
+    const [lineUpdated] = await this.model.update({ inProgress: false }, {
+      where: { id },
+    });
+    if (lineUpdated <= 0) {
+      throw new HttpException('This match has finished', StatusCodes.BAD_REQUEST);
+    }
+    return true;
   }
 }
